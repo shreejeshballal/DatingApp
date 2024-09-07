47e4a3765bf6b0a1608a44efc47c9c5a2f5edaaf
Shreejesh J Ballal <shreejeshballal@gmail.com>
2024-09-07 12:06:01 +0530

    feat: initial dotnet web api setup, install entity framework, create user controller

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+  public DbSet<AppUser> Users { get; set; }
+  
+}
