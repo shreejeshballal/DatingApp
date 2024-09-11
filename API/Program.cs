fd6ea4e57b524be260d39ccd06a72b0ce56e6c7c
Shreejesh J Ballal <shreejeshballal@gmail.com>
2024-09-11 06:44:52 +0530

    init: initial angular client setup, install bootstrap & font-awesome, simple http request

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,17 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapControllers();
+app.UseCors(policy =>
+    policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200")
+);
 
+app.MapControllers();
 app.Run();
